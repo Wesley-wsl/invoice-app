24ff8ba328460996166f43240d772937d5121076
Wesley Westelley <wesleywestelley@gmail.com>
2021-09-30 22:43:01 -0300

    :art: styles: added styles for Header component.

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background-color: #252945;
+  padding: 15px 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    cursor: pointer;
+  }
+
+  p {
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
