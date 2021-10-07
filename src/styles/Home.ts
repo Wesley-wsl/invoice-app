3f5589df4d73ad3373390e0d2ad97012a27ca729
Wesley Westelley <wesleywestelley@gmail.com>
2021-10-07 17:28:21 -0300

    :art: created style for index.tsx

---
+++ src/styles/Home.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    max-width: 800px;
+    margin: 0 auto;
+`;
+export const InvoicesBar = styled.section`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    align-items: center;
+
+    .introducion {
+        h1 {
+            font-size: 40px;
+        }
+
+        p {
+            margin-top: 7px;
+            font-size: 14px;
+        }
+    }
+
+    .actions {
+        display: flex;
+
+        div {
+            padding: 15px;
+        }
+
+        button {
+            background-color: #7c5dfa;
+            padding: 15px;
+            border: none;
+            border-radius: 20px;
+            color: #fff;
+            cursor: pointer;
+            position: relative;
+            padding-left: 45px;
+            span > svg {
+                    background-color: #fff;
+                    position: absolute;
+                    left: 7px;
+                    top: 8px;
+                    padding-left: 10px;
+                    padding-top: 10px;
+                    width: 2rem;
+                    height: 2rem;
+                    border-radius: 50%;
+            }
+        }
+    }
+`;
