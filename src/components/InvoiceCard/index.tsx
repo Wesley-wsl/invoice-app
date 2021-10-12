373d586e687651f627929a2f2b22fc66d1482fac
Wesley Westelley <wesleywestelley@gmail.com>
2021-10-12 19:34:08 -0300

    :wrench: data filled by backend

--- src/components/InvoiceCard/index.tsx
+++ src/components/InvoiceCard/index.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { Container } from "./styles";
 import IconArrowRight from "../../assets/icon-arrow-right.svg";
-import {InvoiceCardProps} from '../../@types/InvoiceTypes';
-
+import { InvoiceCardProps } from "../../@types/InvoiceTypes";
+import Link from "next/link";
 
 const InvoiceCard: React.FC<InvoiceCardProps> = ({ data }) => {
     return (
         <Container>
-            <ul>
-                <li className="id">
-                    <span>#</span>{data.invoiceId}
-                </li>
-                <li className="date">{data.paymentDue}</li>
-                <li className="name">{data.email}</li>
-                <li className="invoice">${data.total}</li>
-                <li className="state">
-                    {" "}
-                    <div /> <p>{data.status}</p>
-                </li>
-                <li className="icon">
-                    <IconArrowRight />
-                </li>
-            </ul>
+            <Link href={`invoice/${data.invoiceId}`}>
+                <ul>
+                    <li className="id">
+                        <span>#</span>
+                        {data.invoiceId}
+                    </li>
+                    <li className="date">{data.paymentDue}</li>
+                    <li className="name">{data.clientName}</li>
+                    <li className="invoice">${data.total}</li>
+                    <li className={`state ${data.status}`}>
+                        {" "}
+                        <div /> <p>{data.status}</p>
+                    </li>
+                    <li className="icon">
+                        <IconArrowRight />
+                    </li>
+                </ul>
+            </Link>
         </Container>
     );
 };
