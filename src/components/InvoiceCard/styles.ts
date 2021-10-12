b4d54017b23f36db8b0fb2890e628b142ba15b45
Wesley Westelley <wesleywestelley@gmail.com>
2021-10-12 19:34:56 -0300

    :wrench: created styles for states in InvoiceCard

--- src/components/InvoiceCard/styles.ts
+++ src/components/InvoiceCard/styles.ts
@@ -8,10 +8,13 @@ export const Container = styled.div`
         display: flex;
         justify-content: space-between;
         list-style: none;
-        background-color: #252945;
-        padding: 30px;
+        background-color: #1e2139;
+        padding: 25px;
         border-radius: 10px;
         align-items: center;
+        height: 84px;
+        transition: 0.3s ease;
+        border: 1px solid transparent;
         &:hover {
             border: 1px solid #7c5dfa;
         }
@@ -20,39 +23,70 @@ export const Container = styled.div`
             color: #888eb0;
         }
 
-        .date, .name {
-            color:#888eb0;
-            font-size: 14px;
+        .id {
+            font-weight: 600;
+        }
+
+        .date,
+        .name {
+            font-size: 13px;
+        }
+
+        .name {
+            overflow: hidden;
+            white-space:nowrap;
+            text-overflow: ellipsis;
+            width: 130px;
         }
 
         .invoice {
             font-size: 18px;
+            width: 75px;
+            overflow: hidden;
+            white-space:nowrap;
+            text-overflow: ellipsis;
+            font-weight: 600;
         }
 
         .state {
             display: flex;
             align-items: center;
+            width: 95px;
+            justify-content: center;
+            border-radius: 6px;
+            padding: 8px;
+            font-weight: 600;
+
+            &.Paid {
+                background-color: #1bff0024;
+
+                div {
+                    background-color: #00ff37;
+                }
+
+                p {
+                    color: #00ff37;
+                }
+            }
+
+            &.Pending {
+                background-color: #ff8f0024;
+
+                div {
+                    background-color: #ff8f00;
+                }
+
+                p {
+                    color: #ff8f00;
+                }
+            }
+            
             div {
                 width: 6px;
                 height: 6px;
-                background-color: #ff8f00;
                 border-radius: 50%;
                 margin-right: 6px;
             }
-
-            p {
-                color: #ff8f00;
-                // Pending: #ff8f00
-                // Paid: #00ff37
-                // Draft: #fff
-            }
-
-            background-color: #ff8f0024;
-            // Pending: #ff8f0024
-            // Paid: #1bff0024
-            // Draft: #111
-            border-radius: 6px;
-            padding: 8px;
         }
     }
 `;
