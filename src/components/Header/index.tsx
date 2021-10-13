c60092d34024b2946ccd011fe3d0ba920f30cfd3
Wesley Westelley <wesleywestelley@gmail.com>
2021-10-13 20:58:09 -0300

    :wrench: change icon when change theme mode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import { HeaderProps } from "../../@types/InvoiceTypes";
 import SunIcon from "../../assets/icon-sun.svg";
+import MoonIcon from "../../assets/icon-moon.svg";
 
 import { Container } from "./styles";
 
-const Header: React.FC<HeaderProps> = ({setLightMode, lightMode}) => {
+const Header: React.FC<HeaderProps> = ({ setLightMode, lightMode }) => {
     return (
         <Container>
             <h1>Invoice-App</h1>
-            <SunIcon onClick={() => setLightMode(lightMode ? false : true)}/>
+            {lightMode ? (
+                <MoonIcon
+                    onClick={() => setLightMode(lightMode ? false : true)}
+                />
+            ) : (
+                <SunIcon
+                    onClick={() => setLightMode(lightMode ? false : true)}
+                />
+            )}
         </Container>
     );
 };
